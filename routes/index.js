5d6b513694ce56457a1a948b697dafb9bb5bdd63
EssemG <bhargav.yogesh@gmail.com>
2017-09-09 15:56:33 -0500

    added code for reading, doesn't work

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,18 @@ var config = {apiKey: "AIzaSyAZmipv-1ohd8x3K521rwd1Av_d5QELSDA",
 /* GET home page. */
 router.get('/', function(req, res, next) {
   firebase.database().ref('users/').set({
-    username: "name",
-    email: "email",
-    profile_picture : "imageUrl"
+    username: "smg",
+    email: "yarubogesh@haHAA.com",
+    profile_picture : "blank"
   });
+
+  firebase.database().ref('users/').once('value').then(function(snapshot){
+    console.log(snapshot.val());
+  }, function(error){
+    console.log("Error: " + error.code);
+  });
+
+
 
   res.render('index', { title: 'Kill me pls' });
 

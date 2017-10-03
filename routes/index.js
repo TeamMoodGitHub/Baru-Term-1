6d4d38f9770f7555c1f7a01deb4cd021eb1d76f4
EssemG <bhargav.yogesh@gmail.com>
2017-10-02 22:37:19 -0500

    almost am able to get numbers from name

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/testAction', function(req, res, next){
+  //console.log(req.query.alarmName);
+  var alarm = req.query.alarmName;
   // var alarmTime = req.query.time;
   // console.log(alarmTime);
   // firebase.database().ref('alarms/').set(alarmTime);
@@ -41,18 +43,34 @@ router.get('/testAction', function(req, res, next){
   // res.send(snapshot.val()["alarms"]);
 
   firebase.database().ref().on("value", function(snapshot){
-    console.log(typeof(snapshot.val()));
-    res.send(snapshot.val()["alarms/" + req.query.getAlarmName]);
+    var retJSON = snapshot.val();
+    console.log("value: " + retJSON.alarms["baru"]);
+    console.log("accessing alarm: " + alarm);
+
+    for(i in retJSON.alarms){
+      //console.log("comparing " + JSON.stringify(i) + " to " + alarm);
+      if(JSON.stringify(i) == alarm){
+        
+        res.send("alarm: " + alarm + ":" + retJSON.alarms[JSON.stringify(i)]);
+      }
+    }
     
+   
+    console.log(retJSON.alarms);
+   
+    
+    // res.send("name: " + alarm + ":" + retJSON.alarms[alarmString]);
+
   })
 
 });
 
-router.get("/newAlarm", function(req, res, next){
+router.post("/newAlarm", function(req, res, next){
   var hour = req.query.hour;
   var minute = req.query.minute;
   var time = (hour + ":" + minute);
   firebase.database().ref('alarms/' + req.query.name).set(time);
   console.log(hour + ":" + minute);
+  res.send("done");
 })
 module.exports = router;
